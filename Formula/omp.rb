b06b6a64c55aae1dec5840d530e1444551ca1ddc
yorha2B0826 <yorha2B0826@users.noreply.github.com>
2026-08-16 23:37:00 +0800

    chore: refresh omp v17.3.5 sha256 (rebuilt with iWAN login tunnel fix)

--- Formula/omp.rb
+++ Formula/omp.rb
@@ -8,7 +8,7 @@ class Omp < Formula
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-darwin-arm64",
           using: :nounzip
-      sha256 "9f3833a26d6421da70569ce18fc5a374994db86fead146bffe4dc4379efdf9c4"
+      sha256 "7d05c51531da4ea273d940f8624ff718e1fe0b7d0787861761083d0fa3909aab"
     end
   end
 
@@ -16,12 +16,12 @@ class Omp < Formula
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-arm64",
           using: :nounzip
-      sha256 "591c15d4e9ca302356a9292019edd28b870233ec2d532011690ec58090f31124"
+      sha256 "dd1bee4a32e882385c08c4617fd7e3fba983b6f25b86e605b02f7a39868d351a"
     end
     on_intel do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-x64",
           using: :nounzip
-      sha256 "3c7157c0ed252159b15bd4a069cf4cc6fd4054a1274bbc45867ad60c8338330c"
+      sha256 "1b72caa581127da91be29396d0e861d03c7f45c8dcd54a8590e5127d56642b8d"
     end
   end
 
