88c44a94bb23ce6ef1931ec3550b3c28b4b64fd5
yorha2B0826 <yorha2B0826@users.noreply.github.com>
2026-08-18 06:00:07 +0800

    chore: update omp to v17.3.7

--- Formula/omp.rb
+++ Formula/omp.rb
@@ -1,14 +1,14 @@
 class Omp < Formula
   desc "Coding agent with the IDE wired in"
   homepage "https://omp.sh"
-  version "17.3.6"
+  version "17.3.7"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-darwin-arm64",
           using: :nounzip
-      sha256 "34fa115639d58832dd7f387c22e592d15c5d9eaa7f48140d48573dd27fa51034"
+      sha256 "f78ca37b55814b763f8f4a2141528541f94d2b4983172c8e5d98133d75081632"
     end
   end
 
@@ -16,12 +16,12 @@ class Omp < Formula
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-arm64",
           using: :nounzip
-      sha256 "31b4a10484ebb0f1663432d7e5c41dbce8827a1457cd0ab666d4227e332ce415"
+      sha256 "f7c157920daee14f769035d99fe7efe5e1d91151978a5f9e534121fd3824d904"
     end
     on_intel do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-x64",
           using: :nounzip
-      sha256 "adf2c0b363c2b8729472c202133123dfb96d69cf4b7e09fb6a5d30c9add95033"
+      sha256 "1a2dc218b891aab5b1aa330b785de791796bddd1038664e9c23451d8da28e9e5"
     end
   end
 
