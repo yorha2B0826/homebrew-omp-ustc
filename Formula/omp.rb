e1bd70f0beafabd0bb13c9667e8997e83e5d3825
yorha2B0826 <yorha2B0826@users.noreply.github.com>
2026-09-14 04:47:34 +0800

    chore: update omp to v18.1.20

--- Formula/omp.rb
+++ Formula/omp.rb
@@ -1,14 +1,14 @@
 class Omp < Formula
   desc "Coding agent with the IDE wired in"
   homepage "https://omp.sh"
-  version "18.1.19"
+  version "18.1.20"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-darwin-arm64",
           using: :nounzip
-      sha256 "74e140ce204dc68d4b323fb7177a13c4b746b2b4eb29d6e969f17c444765c90a"
+      sha256 "c3a2323bd00f20716d033c68d05107dda246c77dfd3042ce3eca314bc67c72f8"
     end
   end
 
@@ -16,12 +16,12 @@ class Omp < Formula
     on_arm do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-arm64",
           using: :nounzip
-      sha256 "3738e77cddc696cf83e808c827884e08e16c612c0ea51f9458866f62f225ebdb"
+      sha256 "e5ca335fd15ba738f03da75cebcd2b9bd9f83a14212b6ade3b3f7ad8946985ba"
     end
     on_intel do
       url "https://github.com/yorha2B0826/oh-my-pi/releases/download/v#{version}/omp-linux-x64",
           using: :nounzip
-      sha256 "cbf12140d9bf73146aa9916d8c786ae3cc12acfafac4835ba842f89dd118dd62"
+      sha256 "16cc9e1b1f956bd4c2be7edaa34911a1f214fefb614c9aa7ba5e55b9aa0728cd"
     end
   end
 
